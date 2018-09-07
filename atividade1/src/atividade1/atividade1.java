bfde40739ef7e4a6b5f2e108af8559853d2c231e
henriquesaviczki <42660048+henriquesaviczki@users.noreply.github.com>
2018-09-07 16:35:41 -0400

    Update atividade1.java

--- atividade1/src/atividade1/atividade1.java
+++ atividade1/src/atividade1/atividade1.java
@@ -1,99 +1,47 @@
 package atividade1;
 import java.util.Scanner;
 
-public class atividade1 {
-    
-    public int[] valor;
-    
-    
-    
-    public void VerificaMaior(int valorMaior){
-        
+public class ValoresArray {
+
+    private int[] dados;
+    private int max = 0;
+    private int min = Integer.MAX_VALUE;
+    private int pos = 0;
+    private double total = 0;
+
+    public ValoresArray(int[] A) {
+        this.dados = A;
     }
-    public void VerificaMenor(int valorMenor){
-        
+
+    public boolean cadastraValor(int v) {
+        if (pos == dados.length) {
+            return false;
+        } else {
+            if (v > max) {
+                max = v;
+            }
+            if (v < min) {
+                min = v;
+            }
+            dados[pos] = v;
+            total += v;
+            pos++;
+            return true;
+        }
     }
-    
-    public void VerificaMedia(int ValorMedia){
-        
+
+    public int getMax() {
+        return max;
     }
-    
-    public void AdicionarNovoValor(int valor){
-        
+
+    public int getMin() {
+        if (pos == 0) return 0;
+        return min;
     }
-    
-    
-    public class Operacao {
-        
-        
-        public int CadastrarValor;
-        public int BuscarMaior;
-        public int BuscaMenor;
-        public int AdicionarNovoValor;
-        
+
+    public double media() {
+        if (pos == 0) return 0;
+        return total / pos;
     }
-    
-        
-   
-    
-    
-    
-    public static void main(String[]args){
-         Scanner input = new Scanner(System.in);
-         
-         int opcao;
-         int i;
-         
-         Opcoes[] opcoes = new Opcoes[5];
-         
-         System.out.println("");
-         
-         System.out.println("=====MENU=====");
-         
-         System.out.println("1. Cadastrar Valor");
-                 
-         System.out.println("2. Buscar Maior");
-         
-         System.out.println("3. Buscar Menor");
-         
-         System.out.println("4. Ler Media");        
-         
-         System.out.println("5. sair");
-         
-         System.out.println("Digite a opcao desejada");
-         
-         opcao = input.nextInt();
-         
-         System.out.println("");
-         
-         if(opcao == 1){
-             for(int i=0; i<5; i++){
-                 
-             System.out.println("_____Cadastros de Valores____" +(i+1)+"____");
-             
-             System.out.println("Valor 1: "); float valor1 = input.nextFloat();
-             
-             System.out.println("Valor 2: "); float valor2 = input.nextFloat();
-             
-             System.out.println("Valor 3: "); float valor3 = input.nextFloat();
-             }
-             }
-             
-         if(opcao == 2){
-               System.out.println("____Buscar Maior____");
-               System.out.println();
-             }
-             
-         if(opcao == 3){
-               System.out.println("____Buscar Menor____");    
-               System.out.print("");
-             
-         }
-         if(opcao == 4){
-               System.out.println("Sair");
-          
-         }
-         if(opcao >= 5){
-               System.out.println("(x) Opcao invalida");
-    }
-    }
+
+}
